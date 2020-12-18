79d9a940e08505af986e226e438ed09704994ceb
Gustavs Jānis Jākobsons <gustavs.jakobsons@gmail.com>
2020-12-18 21:28:50 +0200

    fix

--- server.js
+++ server.js
@@ -31,17 +31,26 @@ mongoose
     app.use("/api/posts", require("./routes/api/posts"));
 
     console.log("Connection to MongoDB established...");
+
+    // Production
+    if (process.env.NODE_ENV === "production") {
+      // Static folder
+      app.use(express.static("client/build"));
+      app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+      });
+    }
   })
   .catch((error) => console.log(error));
 
-// Production
-if (process.env.NODE_ENV === "production") {
-  // Static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// // Production
+// if (process.env.NODE_ENV === "production") {
+//   // Static folder
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 // Server listening port
 app.listen(port, () => {
